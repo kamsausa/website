365e7fd39795e011a9b66c5da8f40b85753f1ec0
kamsausa <kamsa.usa@gmail.com>
2026-08-26 20:21:31 -0400

    Add Supabase configuration

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://eylvmfzkowythuxludkx.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_7GySnJdVM1tGkOb7DaT9Wg_FfH6IHQy";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
